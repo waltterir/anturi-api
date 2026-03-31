8ac08225b0502d11cfa0b0ad9ed26f2cc9427e72
waltterir <riihimakiwaltteri@gmail.com>
2026-03-31 14:45:46 +0300

    feat: get lohko anturit endpoint

--- app/crud/lohkot_crud.py
+++ app/crud/lohkot_crud.py
@@ -10,3 +10,17 @@ def create_lohko(session: Session, lohko_in: LohkoBase):
     session.commit()
     session.refresh(lohko)
     return lohko
+
+def get_lohko_anturit(session: Session, lohko_id: int):
+    lohko = session.get(LohkoDB, lohko_id)
+
+    if not lohko:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Lohko with {lohko_id} not found"
+        )
+
+    return {
+        "id": lohko.id,
+        "anturit": lohko.anturit
+    }
--- app/routes/lohkot.py
+++ app/routes/lohkot.py
@@ -10,3 +10,9 @@ router = APIRouter(prefix="/lohkot", tags=["Lohkot"])
 def create_lohko(*, session: Session = Depends(get_session), lohko_in: LohkoBase):
     return crud.create_lohko(session, lohko_in)
 
+@router.get("/{lohko_id}/anturit")
+def get_lohko_anturit(
+    lohko_id: int,
+    session: Session = Depends(get_session)
+):
+    return crud.get_lohko_anturit(session, lohko_id)
