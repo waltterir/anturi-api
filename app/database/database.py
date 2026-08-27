67cf3c393f2d9a54965eb15b223f6071463abc51
Waltteri Riihimäki <riihimakiwaltteri@gmail.com>
2026-08-27 12:53:18 +0300

    refactor: read DATABASE_URL env var for Postgres support

--- app/database/database.py
+++ app/database/database.py
@@ -1,9 +1,12 @@
+import os
 from sqlmodel import Session, create_engine, SQLModel
 
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database.db")
 
-connect_args = {'check_same_thread': False}
-engine = create_engine("sqlite:///database.db", echo=True, 
-                       connect_args=connect_args)
+# connect_args tarvitaan vain SQLitelle
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
 
 
 def create_db_and_tables():
@@ -11,5 +14,5 @@ def create_db_and_tables():
 
 
 def get_session():
-    with Session(engine) as session: 
+    with Session(engine) as session:
         yield session
