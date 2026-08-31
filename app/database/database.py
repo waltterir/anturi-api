5f7bca2d4d67bfc2dfb24de86ea3ed03b033072b
waltterir <riihimakiwaltteri@gmail.com>
2026-08-31 11:17:41 +0300

    refactor: Corrected grammar in database.py

--- app/database/database.py
+++ app/database/database.py
@@ -3,7 +3,7 @@ from sqlmodel import Session, create_engine, SQLModel
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database.db")
 
-# connect_args tarvitaan vain SQLitelle
+
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
 engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
